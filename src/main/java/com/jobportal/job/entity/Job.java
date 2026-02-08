b9ffcac6c333b34ea308f1bede869920b8d31132
GULSHAN CHOUKSEY <146607501+Gulshan-Chouksey@users.noreply.github.com>
2026-02-08 12:40:01 +0530

    feat: add Job entity

---
+++ src/main/java/com/jobportal/job/entity/Job.java
@@ -0,0 +1,30 @@
+package com.jobportal.job.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String location;
+}
