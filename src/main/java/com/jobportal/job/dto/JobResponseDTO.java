785cf2db09a50ce8c18c5a7e79f84d1c0b6b0f48
GULSHAN CHOUKSEY <146607501+Gulshan-Chouksey@users.noreply.github.com>
2026-02-09 20:20:08 +0530

    feat: add JobResponseDTO

---
+++ src/main/java/com/jobportal/job/dto/JobResponseDTO.java
@@ -0,0 +1,22 @@
+package com.jobportal.job.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobResponseDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    private Integer salaryMin;
+    private Integer salaryMax;
+}
